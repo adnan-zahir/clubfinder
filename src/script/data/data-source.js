0300f030bed36f582250e87c6f6972b74c576d10
adnan-zahir <74122975+adnan-zahir@users.noreply.github.com>
2021-01-11 22:07:03 +0700

    master

---
+++ src/script/data/data-source.js
@@ -0,0 +1,49 @@
+import clubs from "./clubs.js";
+
+/*function DataSource(onSuccess, onFailed) {
+    this.onSuccess = onSuccess;
+    this.onFailed = onFailed;
+}
+
+DataSource.prototype.searchClub = function (keyword) {
+    const filteredClubs = clubs.filter(club => club.name.toUpperCase().includes(keyword.toUpperCase()));
+
+    if (filteredClubs.length) {
+        this.onSuccess(filteredClubs);
+    } else {
+        //this.onFailed(keyword + " is not found");
+        this.onFailed(`${keyword} is not found`)
+    }
+};*/
+
+/*class DataSource {
+    constructor(onSuccess, onFailed) {
+        this.onSuccess = onSuccess;
+        this.onFailed = onFailed;
+    }
+
+    searchClub(keyword) {
+        const filteredClubs = clubs.filter(club => club.name.toUpperCase().includes(keyword.toUpperCase()));
+
+        if (filteredClubs.length) {
+            this.onSuccess(filteredClubs);
+        } else {
+            this.onFailed(`${keyword} is not found`);
+        }
+    }
+}*/
+
+class DataSource {
+    static searchClub(keyword) {
+        return new Promise((resolve, reject) => {
+            const filteredClubs = clubs.filter(club => club.name.toUpperCase().includes(keyword.toUpperCase()));
+
+            if (filteredClubs.length)
+                resolve(filteredClubs)
+            else
+                reject(`${keyword} is not found`)
+        })
+    }
+}
+
+export default DataSource;
